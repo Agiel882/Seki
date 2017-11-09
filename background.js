3fb2c8a8332cbf71899f2501d4cba5bf7c19c8ad
Agiel882 <agiel882@gmail.com>
2017-11-08 20:18:35 -0600

    quick changes and comments.

--- background.js
+++ background.js
@@ -1,1 +1,1 @@
-	
+//This background script exists solely to allow the extension to always be running without using a popup.
--- frame.js
+++ frame.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var button = document.getElementById("KanjiButton");
   button.addEventListener('click', function() {
 	var Kanji = document.getElementById('KanjiBox').value;
+	//find the active tab and send the script to it. Execute script and send the kanji to it.
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   		chrome.tabs.executeScript(tabs[0].id, {file: 'seki.js'}, function() {
 			chrome.tabs.sendMessage(tabs[0].id, Kanji);
